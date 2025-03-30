93d8fb152ee397c76987a14e1bce3896e6e973f1
Nuz2002 <nazarbekovnavruz@gmail.com>
2025-03-30 17:52:39 +0600

    not important

--- ForumBackend/src/main/java/com/example/ForumBackend/service/EmailService.java
+++ ForumBackend/src/main/java/com/example/ForumBackend/service/EmailService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
+    // This folders needs some change after front-end deployment
+
     @Autowired
     private JavaMailSender mailSender;
 
